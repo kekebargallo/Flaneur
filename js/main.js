23cc7fd63071fc32a23f683bd80df82030da0638
keke bargallo <kekebargallo@gmail.com>
2022-06-28 22:44:22 -0300

    Buscador añadido al shop

--- js/main.js
+++ js/main.js
@@ -1,115 +1,19 @@
-let prenda = prompt("Elige prenda A, B o C");
-let todoOk = true;
-let confirmacion;
-let valorPrenda;
+function filtroString(arr){
+    return filtro => arr.filter(elemento => elemento.nombre.includes(filtro))
+};
 
-switch(prenda.toUpperCase()){
-    case "A":
-        confirmacion = prompt("Elegiste A? Escribe si o no.");
-        if(confirmacion.toUpperCase() === "SI"){
-            valorPrenda = 1500;
-            alert(`Perfecto, el precio es de ${valorPrenda}$`);
-        }else if(confirmacion.toUpperCase() === "NO"){
-            alert("Lo sentimos. Vuelve a intentar.");
-            todoOk = false;
-        }else{
-            alert("Vuelve a intentar.");
-            todoOk = false;
-        }
-        break;
-    case "B":
-        confirmacion = prompt("Elegiste B? Escribe si o no.");
-        if(confirmacion.toUpperCase() === "SI"){
-            valorPrenda = 2000;
-            alert(`Perfecto, el precio es de ${valorPrenda}$`);
-        }else if(confirmacion.toUpperCase() === "NO"){
-            alert("Lo sentimos. Vuelve a intentar.");
-            todoOk = false;
-        }else{
-            alert("Vuelve a intentar.");
-            todoOk = false;
-        }
-        break;
-    case "C":
-        confirmacion = prompt("Elegiste C? Escribe si o no.");
-        if(confirmacion.toUpperCase() === "SI"){
-            valorPrenda = 3000;
-            alert(`Perfecto, el precio es de ${valorPrenda}$`)
-        }else if(confirmacion.toUpperCase() === "NO"){
-            alert("Lo sentimos. Vuelve a intentar.");
-            todoOk = false;
-        }else{
-            alert("Vuelve a intentar.");
-            todoOk = false;
-        }
-        break;
-    default:
-        alert("Vuelve a intentar.");
-        todoOk = false;
-        break;
+function filtroNumero(arr){
+    return filtro => arr.filter(elemento => elemento.precio < filtro)
 }
 
-while(todoOk === false){
-    let prenda = prompt("Elige prenda A, B o C");
-    todoOk = true;
-    switch(prenda.toUpperCase()){
-        case "A":
-            confirmacion = prompt("Elegiste A? Escribe si o no.");
-            if(confirmacion.toUpperCase() === "SI"){
-                valorPrenda = 1500;
-                alert(`Perfecto, el precio es de ${valorPrenda}$`);
-            }else if(confirmacion.toUpperCase() === "NO"){
-                alert("Lo sentimos. Vuelve a intentar.");
-                todoOk = false;
-            }else{
-                alert("Vuelve a intentar.");
-                todoOk = false;
-            }
-            break;
-        case "B":
-            confirmacion = prompt("Elegiste B? Escribe si o no.");
-            if(confirmacion.toUpperCase() === "SI"){
-                valorPrenda = 2000;
-                alert(`Perfecto, el precio es de ${valorPrenda}$`);
-            }else if(confirmacion.toUpperCase() === "NO"){
-                alert("Lo sentimos. Vuelve a intentar.");
-                todoOk = false;
-            }else{
-                alert("Vuelve a intentar.");
-                todoOk = false;
-            }
-            break;
-        case "C":
-            confirmacion = prompt("Elegiste C? Escribe si o no.");
-            if(confirmacion.toUpperCase() === "SI"){
-                valorPrenda = 3000;
-                alert(`Perfecto, el precio es de ${valorPrenda}$`);
-            }else if(confirmacion.toUpperCase() === "NO"){
-                alert("Lo sentimos. Vuelve a intentar.");
-                todoOk = false;
-            }else{
-                alert("Vuelve a intentar.");
-                todoOk = false;
-            }
-            break;
-        default:
-            alert("Vuelve a intentar.");
-            todoOk = false;
-            break;
-    }
-}
+const productos = [
+    {nombre: "remeraCorta", precio: 3000, talles: 3, stock: 3},
+    {nombre: "camisaSinMangas", precio: 2500, talles: 3, stock: 3},
+    {nombre: "pantalon", precio: 4000, talles: 2, stock: 2},
+    {nombre: "cinturon", precio: 1200, talles: 1, stock: 3}
+];
 
-todoOk = true;
+let filtroProductosS = filtroString(productos);
+let filtroProductosN = filtroNumero(productos);
 
-for(let i = 0; i <= 2; i++){
-    unidades = parseInt(prompt("Cuantas prendas quiere. Escriba el número."));
-    if(Number.isInteger(unidades) == true){
-        i = 4;
-    }else if(Number.isInteger(unidades) == false){
-        alert("Vuelva a intentar colocando un número.");
-    }else if(Number.isInteger(unidades) == false && i === 3)
-        alert("Recargue la página");
-        todoOk = false;
-}
 
-alert("El precio de su pedido es de " + valorPrenda * unidades + "$.")
