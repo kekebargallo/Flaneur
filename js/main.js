171c070e958fdbdc5c93fa35e8c9ed325ac43c28
keke bargallo <kekebargallo@gmail.com>
2022-06-29 02:09:59 -0300

    avances con js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,59 @@ function filtroNumero(arr){
     return filtro => arr.filter(elemento => elemento.precio < filtro)
 }
 
+function ordenarAbc(arr){
+    let obj = [];
+    let arrOrdenado = [];
+    arr.forEach((elemento) => {
+        nuevoElemento = elemento.nombre;
+        obj.push(nuevoElemento);
+    });
+    objOrdenado = obj.sort();
+    objOrdenado.forEach(elemento => {
+        arrOrdenado.push(arr.find(i => i.nombre === elemento))
+    })
+    return arrOrdenado;
+}
+
+function ordenarNumeros(arr, valor){
+    let obj = [];
+    let arrOrdenado = [];
+    arr.forEach((elemento) => {
+        switch(valor) {
+            case "precio":
+                nuevoElemento = elemento.precio;
+                obj.push(nuevoElemento);
+                break;
+            case "stock":
+                nuevoElemento = elemento.stock;
+                obj.push(nuevoElemento);
+                break;
+            case "talles":
+                nuevoElemento = elemento.talles;
+                obj.push(nuevoElemento);
+                break;
+        }
+    });
+    objOrdenado = obj.sort((a,b) => a-b);
+    let comprobador;
+    objOrdenado.forEach(elemento => {
+        switch(valor) {
+            case "precio":
+                comprobador = arrOrdenado.some(e1 => e1 === elemento);
+                arrOrdenado.push(arr.find(e2 => e2.precio === elemento));
+                break;
+            case "stock":
+                arrOrdenado.push(arr.find(e2 => e2.talles === elemento));
+                break;
+            case "talles":
+                comprobador = arrOrdenado.some(e1 => e1 === elemento);
+                arrOrdenado.push(arr.find(e2 => e2.talles === elemento));
+                break;
+        }
+    })
+    return arrOrdenado;
+}
+
 const productos = [
     {nombre: "remeraCorta", precio: 3000, talles: 3, stock: 3},
     {nombre: "camisaSinMangas", precio: 2500, talles: 3, stock: 3},
@@ -15,5 +68,7 @@ const productos = [
 
 let filtroProductosS = filtroString(productos);
 let filtroProductosN = filtroNumero(productos);
+let productosOrdenadosAbc = ordenarAbc(productos);
+let productosOrdenadosCba = productosOrdenadosAbc.reverse();
 
-
+console.log(ordenarNumeros(productos, "talles"));
