d3c472dbcc49bbba3261db3b325b527a65da6cc7
keke bargallo <kekebargallo@gmail.com>
2022-07-19 02:58:50 -0300

    agregada libreria SweetAlert

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ const producto = (n, p, t, s) => {
 const filtro = (e) => {
     let productosFiltrados = [];
     localStorage.setItem("productosLS", JSON.stringify(productos));
-    let productosLS = JSON.parse(localStorage.getItem("productosLS"));
+    productosLS = JSON.parse(localStorage.getItem("productosLS"));
     if(e.key === "Enter"){
         if(shopFiltro.value != ""){
             let productosFiltrados2 = [];
@@ -87,21 +87,46 @@ const filtro = (e) => {
 }
 
 const menorPrecio = () => {
-    let productosLS = JSON.parse(localStorage.getItem("productosLS"));
+    productosLS = JSON.parse(localStorage.getItem("productosLS"));
     let arrOrdenado = ordenarNumeros(productosLS, "precio");
     cargarProductos(arrOrdenado.reverse());
 };
 
 const mayorPrecio = () => {
-    let productosLS = JSON.parse(localStorage.getItem("productosLS"));
+    productosLS = JSON.parse(localStorage.getItem("productosLS"));
     let arrOrdenado = ordenarNumeros(productosLS, "precio");
     cargarProductos(arrOrdenado);
 };
 
 const restaurarOrden = () => {
-    let productosLS = JSON.parse(localStorage.getItem("productosLS"));
+    productosLS = JSON.parse(localStorage.getItem("productosLS"));
     cargarProductos(productosLS);
 };
+
+const agregarAlCarrito = () => {
+    Swal.fire({
+        focusConfirm: false,
+        title: '¿Quiere agregar este elemento al carrito?',
+        text: false,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: 'grey',
+        cancelButtonColor: 'grey',
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Cancelar',
+        focusConfirm: false
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Se agregó el elemento al carrito.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+      })
+}
 
 
 const productos = [
@@ -112,38 +137,43 @@ const productos = [
 ];
 
 localStorage.setItem("productosLS", JSON.stringify(productos));
+let productosLS = JSON.parse(localStorage.getItem("productosLS"))
 
 const shopGaleria = document.getElementById("shopGaleria");
 const shopBuscador = document.getElementById("shopBuscador");
 shopBuscador.addEventListener("keydown", filtro);
+
 const shopFiltro = document.getElementById("shopFiltro");
 shopFiltro.addEventListener("keydown", filtro);
+
 const menorPrecioShop = document.getElementById("menorPrecioShop");
 menorPrecioShop.addEventListener("mouseup", menorPrecio);
+
 const mayorPrecioShop = document.getElementById("mayorPrecioShop");
 mayorPrecioShop.addEventListener("mouseup", mayorPrecio);
+
 const restaurarOrdenShop = document.getElementById("restaurarOrdenShop");
 restaurarOrdenShop.addEventListener("mouseup", restaurarOrden);
-
 
 const cargarProductos = (arr) => {
     shopGaleria.innerHTML = "";
     arr.forEach(elemento => {
+        let {img: {primeraImg, segundaImg, terceraImg}} = elemento;
         let card = document.createElement("article");
         card.setAttribute("class", "shop__galeria-card");
         card.innerHTML = `<div id="carouselExampleControlsNoTouching${elemento.id}" class="carousel slide shop__galeria-card-carrusel" data-bs-touch="false" data-bs-interval="false">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="${elemento.img.primeraImg}" class="d-block w-100" alt=${elemento.nombre}>
+                    <img src="${primeraImg}" class="d-block w-100" alt=${elemento.nombre}>
                 </div>
                 <div class="carousel-item">
-                    <img src="${elemento.img.segundaImg}" class="d-block w-100" alt=${elemento.nombre}>
+                    <img src="${segundaImg}" class="d-block w-100" alt=${elemento.nombre}>
                 </div>
                 <div class="carousel-item">
-                    <img src="${elemento.img.terceraImg}" class="d-block w-100" alt="${elemento.nombre}">
+                    <img src="${terceraImg}" class="d-block w-100" alt="${elemento.nombre}">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching${elemento.id}" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
@@ -159,10 +189,22 @@ const cargarProductos = (arr) => {
             <label>M <input type="radio" name="talle" value="medium"></label>
             <label>L <input type="radio" name="talle" value="large"></label>
         </div>
-        <a class="shop__galeria-card-boton" id="comprar${elemento.id}"><span>COMPRAR</span></a>`
+        <a class="shop__galeria-card-boton" id="comprar_${elemento.id}"><span>COMPRAR</span></a>`
     shopGaleria.appendChild(card);
     })
 };
 
-cargarProductos(productos);
+cargarProductos(productosLS);
+
+let comprarRemBtn = document.getElementById("comprar_remCorta");
+comprarRemBtn.addEventListener("mouseup", agregarAlCarrito);
+
+let comprarCamisaBtn = document.getElementById("comprar_camisa");
+comprarCamisaBtn.addEventListener("mouseup", agregarAlCarrito);
+
+let comprarPantalonBtn = document.getElementById("comprar_pantalon");
+comprarPantalonBtn.addEventListener("mouseup", agregarAlCarrito);
+
+let comprarCinturonBtn = document.getElementById("comprar_cinturon");
+comprarCinturonBtn.addEventListener("mouseup", agregarAlCarrito);
 
